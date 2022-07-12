11dbc206df1c0b4b2aa8825dda0069cb53119ab6
devHudi <devhudi@gmail.com>
2022-07-12 17:48:20 +0900

    설정 변경

--- blog-config.js
+++ blog-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "dal.log",
   description: "우아한테크코스 4기 달록팀 기술 블로그입니다.",
   author: "dallog",
-  siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
+  siteUrl: "https://dallog.github.io/",
   links: {
     github: "https://github.com/woowacourse-teams/2022-dallog",
   },
